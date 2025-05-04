b57880088a5e479233acdae809d6aafd51184daf
sbt899221808 <108788000+sbt899221808@users.noreply.github.com>
2025-05-04 13:16:34 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('index.html')
